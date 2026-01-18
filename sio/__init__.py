f84f051bc6a03ba3a427b92fab3407806759dd0d
Hisham Mahmood <hishammahmood41@gmail.com>
2026-01-18 10:08:27 +0500

    Export only SocketIOConsumer from root package.

--- sio/__init__.py
+++ sio/__init__.py
@@ -1,11 +1,7 @@
 # __init__.py (root package)
 import logging
 
-from .socketio.constants import DEFAULT_NAMESPACE, SOCKET_IO_VERSION
-from .socketio.socket import NamespaceSocket
-from .socketio.protocol import SocketIOPacket, SocketIOParser
-from .socketio.server import SocketIOServer, get_socketio_server
-from .consumer import SocketIOConsumer, get_socketio_server
+from .consumer import SocketIOConsumer
 
 __version__ = "v0.1.0"
 
@@ -14,18 +10,9 @@ logger.info(
     "sio root package imported",
     extra={
         "version": __version__,
-        "socketio_version": SOCKET_IO_VERSION,
-        "default_namespace": DEFAULT_NAMESPACE,
     },
 )
 
 __all__ = [
-    "DEFAULT_NAMESPACE",
-    "SOCKET_IO_VERSION",
-    "NamespaceSocket",
     "SocketIOConsumer",
-    "SocketIOPacket",
-    "SocketIOParser",
-    "SocketIOServer",
-    "get_socketio_server",
 ]
